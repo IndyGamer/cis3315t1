db309d22a3346c372aab064e363da0f1f16ae1c9
55allenjn05 <55allenjn05@MUP-R-EH207L34.PASSHE.LCL>
2017-02-28 09:52:40 -0500

    yup

--- src/part3/T1P3.java
+++ src/part3/T1P3.java
@@ -1,5 +1,10 @@
 package part3;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
 /**
  * Write 100 random ints to a text file; one int per line. Use a try with
  * resources and a catch block as needed. Add a finally block that prints the
@@ -10,6 +15,24 @@ package part3;
 public class T1P3 {
 
     public static void main(String[] args) {
-
+            Random rand = new Random();
+            try {
+                File Here = new File("here.txt");
+                if (!Here.exists()) {
+                    Here.createNewFile();
+                }
+                int r = rand.nextInt(100)+1;
+                FileWriter wr = new FileWriter(Here);
+                for (int i = 1; i <= 100; i++) {
+                    r = r * i;
+                    wr.write("r + rand.nextInt(100)+1)");
+                }
+                wr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                System.out.println("All Done");
+            }
+        }
     }
-}
